2ee67701329e17eba9d2eab7cdc3d82b9f03cf1e
Joy Biswas <joy.blanks@hotmail.com>
2018-10-17 05:22:30 +0530

    Exchange Module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { BrowserRouter, Route } from 'react-router-dom';
+import store from "./js/store/index";
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import ExchangeWindow from './js/containers/ExchangeWindow';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <Route path="/" component={ExchangeWindow}/>
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
